306fcf6aaa382c3ddd4f5986c7dc72b11c58f193
Tobias Brander <tobias.sundstrand@gmail.com>
2018-10-08 19:25:56 +0200

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Tobias Brander on 2018-10-08.
+//  Copyright © 2018 Tobias Brander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
